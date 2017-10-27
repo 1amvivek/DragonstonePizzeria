7373cebf5e2480dbab161cb695657ea10b460037
Y.Arun Ramanathan <y.arunram@gmail.com>
2017-10-27 15:11:31 -0700

    Adding "Add quantity", "Reduce Quantity" features to cart
    "AddQuantity", "ReduceQuantity" messages added to socket to handle
    user's manipulations in the cart.

--- DragonStone UI/js/cart.js
+++ DragonStone UI/js/cart.js
@@ -2,7 +2,7 @@ var socket = io.connect('http://localhost:8080');
 var app = angular.module("myShoppingList", []); 
 app.controller("myCtrl", function($scope) {
     $scope.quantity = 1;
-    $scope.totalPrice = "$10";
+    $scope.totalPrice = "$58";
     $scope.products = [{name: "Hound's Chicken", quantity: 1, price : "$10"},{name: "Fire and Blood spicy pizza", quantity: 1, price : "$12"},{name: "Spices are coming", quantity: 1, price : "$16"},{name: "Winterfell special", quantity: 1, price : "$20"}];
     //$scope.products =  ["Hound's Chicken", "Fire and Blood spicy pizza", "Spices are coming", "Winterfell special"];
   
@@ -14,15 +14,32 @@ app.controller("myCtrl", function($scope) {
        console.log('add quantity' + x)
        socket.emit('addQuantity', { pizza: x }); 
     }
-  /*  $scope.calculateTotal = function{
-        for(var pizza in $scope.products)
-        {
 
-        }
-    }*/
+    $scope.reduceQuantity = function(x){
+       console.log('reduce quantity' + x)
+       if($scope.products[x].quantity>1)
+         socket.emit('reduceQuantity', { pizza: x }); 
+    }
+
+
     socket.on('removePizza', function (data) {
-        console.log('message from server' + data.pizza);
+        console.log('message from server' + data.pizza + "price: " + data.totalPrice);
         $scope.products.splice(data.pizza, 1);
+        $scope.totalPrice = data.totalPrice;
+        $scope.$apply();
+       });
+
+    socket.on('addQuantity', function (data) {
+        console.log('message from server - addQuantity:' + data.pizza);
+        $scope.products[data.pizza].quantity++;
+        $scope.totalPrice = data.totalPrice;
+        $scope.$apply();
+       });
+
+    socket.on('reduceQuantity', function (data) {
+        console.log('message from server - reduceQuantity:' + data.pizza);
+        $scope.products[data.pizza].quantity--;
+        $scope.totalPrice = data.totalPrice;
         $scope.$apply();
        });
 
--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var totalPrice = "$40";
 
 server.listen(8080, function() {
   console.log('Server running at http://127.0.0.1:8080/');
@@ -12,9 +13,25 @@ io.sockets.on('connection', function(socket) {
    
   socket.on('removePizza', function (data) {
       console.log(data.pizza);
-      io.sockets.emit('removePizza', { pizza: data.pizza });
+      //todo: rest call to golang pizza api
+      io.sockets.emit('removePizza', { pizza: data.pizza,totalPrice:totalPrice });
    
     });
+
+  socket.on('addQuantity', function (data) {
+      console.log(data.pizza);
+      //todo: rest call to golang pizza api
+      io.sockets.emit('addQuantity', { pizza: data.pizza,totalPrice:totalPrice });
+   
+    });
+
+  socket.on('reduceQuantity', function (data) {
+      console.log(data.pizza);
+      //todo: rest call to golang pizza api
+      io.sockets.emit('reduceQuantity', { pizza: data.pizza,totalPrice:totalPrice });
+   
+    });
+
 
   socket.on('closeConnection',function(){
       console.log('Client disconnects'  + socket.id);
