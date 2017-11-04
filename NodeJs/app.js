203d9d6a60d609e57479effe210b04f2b0a8e405
Pranav Prem <pranavprem93@gmail.com>
2017-11-03 22:16:19 -0700

    Added socket connections for handling product catalog and activity logs

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -2,10 +2,12 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var totalPrice = "$40";
-
-
 //Handle from post data
 var bodyParser = require('body-parser');
+
+var path = require('path'); 
+var catalog;
+var connections = [];
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
@@ -17,20 +19,18 @@ app.use(function(req, res, next) {
 });
 
 
-
-
 app.post('/login', function (req, res, next) {
-	var email = req.body.email;
+  var email = req.body.email;
         var password = req.body.password;
 
-	
-			if (req.body.email && req.body.email=== email && req.body.password && req.body.password === password) {
-				req.session.authenticated = true;
-				res.redirect('/secure');
-			} else {
-				req.flash('error', 'Username and password are incorrect');
-				
-			}
+  
+      if (req.body.email && req.body.email=== email && req.body.password && req.body.password === password) {
+        req.session.authenticated = true;
+        res.redirect('/secure');
+      } else {
+        req.flash('error', 'Username and password are incorrect');
+        
+      }
    res.send('username sent to Node Server: "' + email + '".'+ '<br/> password sent to Node Server: "' + password + '".');
    response = {
       username:email,
@@ -40,42 +40,42 @@ app.post('/login', function (req, res, next) {
 
    console.log(response);
 
-		});
+    });
 
 // Register User
 app.post('/register', function(req, res){
-	var name = req.body.name;
-	var email = req.body.email;
-	var password = req.body.password;
-	var password2 = req.body.cpassword;
+  var name = req.body.name;
+  var email = req.body.email;
+  var password = req.body.password;
+  var password2 = req.body.cpassword;
 
-	// Validation
-	req.checkBody('name', 'Name is required').notEmpty();
-	req.checkBody('email', 'Email is required').notEmpty();
-	req.checkBody('email', 'Email is not valid').isEmail();
-	req.checkBody('password', 'Password is required').notEmpty();
-	req.checkBody('cpassword', 'Passwords do not match').equals(req.body.password);
+  // Validation
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('cpassword', 'Passwords do not match').equals(req.body.password);
 
-	var errors = req.validationErrors();
+  var errors = req.validationErrors();
 
-	if(errors){
-		res.render('register',{
-			errors:errors
-		});
-	} else {
-		var newUser = new User({
-			name: name,
-			email:email,
-			password: password
-		});
+  if(errors){
+    res.render('register',{
+      errors:errors
+    });
+  } else {
+    var newUser = new User({
+      name: name,
+      email:email,
+      password: password
+    });
 
-		User.createUser(newUser, function(err, user){
-			if(err) throw err;
-			console.log(user);
-		});
+    User.createUser(newUser, function(err, user){
+      if(err) throw err;
+      console.log(user);
+    });
 
-		req.flash('success_msg', 'You are registered and can now login');
-	}
+    req.flash('success_msg', 'You are registered and can now login');
+  }
 
 res.send('You are registered successfully');
    
@@ -88,39 +88,74 @@ server.listen(8080, function() {
   console.log('Server running at http://127.0.0.1:8080/');
 });
 
+//handle socket connections
 io.sockets.on('connection', function(socket) {
   
   console.log('new client:' + socket.id);
+  connections.push(socket.id); 
+  io.sockets.emit('join', {user : socket.id });
    
-  socket.on('removePizza', function (data) {
-      console.log(data.pizza);
+
+  socket.on('addPizza', function (data) {
+      console.log(data.pizzaId);
       //todo: rest call to golang pizza api
-      io.sockets.emit('removePizza', { pizza: data.pizza,totalPrice:totalPrice });
+      io.sockets.emit('addPizza',{pizzaId:data.pizzaId,totalPrice:totalPrice,user : socket.id});
+   
+    });
+ 
+  socket.on('removePizza', function (data) {
+      console.log(data.pizzaId);
+      //todo: rest call to golang pizza api
+      io.sockets.emit('removePizza', { pizzaId: data.pizzaId,totalPrice:totalPrice,user : socket.id });
    
     });
 
   socket.on('addQuantity', function (data) {
-      console.log(data.pizza);
+      console.log(data.pizzaId);
       //todo: rest call to golang pizza api
-      io.sockets.emit('addQuantity', { pizza: data.pizza,totalPrice:totalPrice });
+      io.sockets.emit('addQuantity', { pizzaId: data.pizzaId,totalPrice:totalPrice,user : socket.id });
    
     });
 
   socket.on('reduceQuantity', function (data) {
-      console.log(data.pizza);
+      console.log(data.pizzaId);
       //todo: rest call to golang pizza api
-      io.sockets.emit('reduceQuantity', { pizza: data.pizza,totalPrice:totalPrice });
+      io.sockets.emit('reduceQuantity', { pizzaId: data.pizzaId,totalPrice:totalPrice,user : socket.id });
    
     });
 
+  socket.on('lookingAt',function(data){
+      //todo: rest call to golang pizza api
+      io.sockets.emit('lookingAt', { pizzaId: data.pizzaId,user : socket.id });
+  });
+
+  socket.on('getCatalog',function(){
+    catalog = [{id:'0',name: "Pepperoni pizza", price : "$12",img_url:"img/product/1.jpg",desc:"This is a medium spicy Pepperoni Pizza with Tomato sauce, triple Pepperoni and mozzarella cheese."},
+    {id:'1',name: "Pizza 2", price : "$10",img_url:"img/product/2.jpg",desc:"This is a medium spicy pizza 2."},
+    {id:'2',name: "Pizza 3", price : "$14",img_url:"img/product/3.jpg",desc:"This is a medium spicy pizza 3."},
+    {id:'3',name: "Pizza 4", price : "$15",img_url:"img/product/4.jpg",desc:"This is a medium spicy pizza 4."},
+    {id:'4',name: "Pizza 5", price : "$18",img_url:"img/product/5.jpg",desc:"This is a medium spicy pizza 5."}];
+    socket.emit('catalog',{catalog:catalog,user : socket.id});
+    console.log('sent catalog');
+  });
 
   socket.on('closeConnection',function(){
       console.log('Client disconnects'  + socket.id);
       socket.disconnect();
+      removePlayer(socket.id);
+      io.sockets.emit('left', {user : socket.id });
   });
 
   socket.on('disconnect', function() {
       console.log('Got disconnected!'  + socket.id);
       socket.disconnect();
+      io.sockets.emit('left', {user : socket.id });
+      removePlayer(socket.id);
    });
 });
+
+function removePlayer(item)
+{
+var index = connections.indexOf(item);
+connections.splice(index, 1);
+}
