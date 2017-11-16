413fb8f7958ff43e6c526f2f219ad6e3747657f7
1amvivek <viveklakshmanan@live.com>
2017-11-16 00:01:33 -0800

    Apriori Algorithm implementation

--- GoAPI/src/goapi/server.go
+++ GoAPI/src/goapi/server.go
@@ -23,6 +23,10 @@ var mongodb_database = "cmpe281"
 var mongodb_collection = "redistest"
 var i = 0
 var servers = []string{mongodb_server1, mongodb_server2, mongodb_server3}
+<<<<<<< Updated upstream
+=======
+//cassandra_server
+>>>>>>> Stashed changes
 
 type (
 	// User represents the structure of our resource
@@ -50,7 +54,11 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 	mx.HandleFunc("/orders/{order_id}", getHandler(formatter)).Methods("GET")
 	mx.HandleFunc("/order", postHandler(formatter)).Methods("POST")
 	mx.HandleFunc("/order", putHandler(formatter)).Methods("PUT")
+<<<<<<< Updated upstream
 	mx.HandleFunc("/order/{order_id}", deleteHandler(formatter)).Methods("DELETE")
+=======
+	mx.HandleFunc("/order", deleteHandler(formatter)).Methods("DELETE")
+>>>>>>> Stashed changes
 }
 
 func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
@@ -328,6 +336,7 @@ func deleteHandler(formatter *render.Render) http.HandlerFunc {
 	}
 }
 
+<<<<<<< Updated upstream
 func putHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 
@@ -362,5 +371,92 @@ func putHandler(formatter *render.Render) http.HandlerFunc {
 			updateHelper(value, user)
 		}
 
+=======
+
+func deleteHandler(formatter *render.Render) http.HandlerFunc {  
+    return func(w http.ResponseWriter, req *http.Request) {
+	
+	var id Id 
+	//get mongodb connection
+    decoder := json.NewDecoder(req.Body)
+    err1 := decoder.Decode(&id)
+    server_val := Balance()
+
+	//connect to redis
+	conn,_, user := connectToRedis(redis_connect, id.SerialNumber)
+
+	//deleting in redis
+	if user.SerialNumber != "" {
+			fmt.Println("Deleting values at Redis End")			
+			//delete in redis
+			conn.Cmd("DEL", id.SerialNumber)
+	} else {
+			fmt.Println("There aren't any values in Redis")
+	}
+
+	// It wont delete the data from redis and mongo, as the server_val is not properly set. Hence we have to run del again.
+    s := getSession(server_val)
+    defer s.Close()
+    fmt.Println("Deleting the user")
+    
+    if err1 != nil {
+        ErrorWithJSON(w, "Incorrect body", http.StatusBadRequest)
+        fmt.Println(err1)
+        return
+    }
+
+    //deleting in mongo
+    c := s.DB(mongodb_database).C(mongodb_collection)
+    err2 := c.Remove(id)
+
+    if err2 != nil {
+    	fmt.Println("Some Random error")
+    	return
+    	}
+	}
+}
+
+
+func putHandler(formatter *render.Render) http.HandlerFunc {  
+    return func(w http.ResponseWriter, req *http.Request) {
+	
+	var user User 
+	//get mongodb connection
+    decoder := json.NewDecoder(req.Body)
+    err1 := decoder.Decode(&user)
+    server_val := Balance()
+
+	//connect to redis
+	conn,_, name := connectToRedis(redis_connect, user.SerialNumber)
+
+	//deleting in redis
+	if name.SerialNumber != "" {
+			fmt.Println("Deleting values at Redis End")			
+			//delete in redis
+			conn.Cmd("DEL", name.SerialNumber)
+	} else {
+			fmt.Println("There aren't any values in Redis")
+	}
+
+    s := getSession(server_val)
+    defer s.Close()
+    fmt.Println("Updating the user")
+    
+    if err1 != nil {
+        ErrorWithJSON(w, "Incorrect body", http.StatusBadRequest)
+        fmt.Println(err1)
+        return
+    }
+    fmt.Println(user.SerialNumber)
+
+	// It wont update the data from redis and mongo at the same time, as the server_val is not properly set. Hence we have to run update command again.
+    c := s.DB(mongodb_database).C(mongodb_collection)
+    err2 := c.Update(bson.M{"serialnumber": user.SerialNumber}, bson.M{"$set": bson.M{"name": user.Name}})
+
+    if err2 != nil {
+    	fmt.Println("Some Random error")
+    	return
+    	}
+>>>>>>> Stashed changes
 	}
 }
